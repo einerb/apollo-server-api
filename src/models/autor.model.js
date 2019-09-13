f7a4de912225f6b595aca5071d035ead3f785b3d
Einer Bravo <einerbravo@gmail.com>
2019-09-13 07:26:17 -0500

    Minor changes.

--- src/models/autor.model.js
+++ src/models/autor.model.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const AutorSchema = mongoose.Schema({
   first_name: { type: String, required: [true, "Obligatory field"] },
-  last_name: { type: String, required: [true, "Obligatory field"] },
+  last_name: { type: String },
   country: { type: String }
 });
 
