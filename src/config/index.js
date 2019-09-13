f52915f2003a13e166cfa5b747a4f4a470fa3008
Einer Bravo <einerbravo@gmail.com>
2019-09-13 13:32:56 -0500

    Add connection to MongoDB.

--- src/config/index.js
+++ src/config/index.js
@@ -7,11 +7,6 @@ let config = {}; //Make this global to use all over the application
 config.app = process.env.APP || "development";
 config.port = process.env.PORT || "4000";
 
-config.db_dialect = process.env.DB_DIALECT || "mongodb";
-config.db_host = process.env.DB_HOST || "localhost";
-config.db_name = process.env.DB_NAME || "apollo-server-development";
-config.db_port = process.env.DB_PORT || "27017";
-
 config.jwt_encryption = process.env.JWT_ENCRYPTION || "|Z2RM*LUFYebto;-ZbXo";
 config.jwt_expiration = process.env.JWT_EXPIRATION || "10800";
 
--- src/index.js
+++ src/index.js
@@ -1,16 +1,49 @@
 import express from "express";
+import mongoose from "mongoose";
 import { ApolloServer } from "apollo-server-express";
+import { MongoMemoryServer } from "mongodb-memory-server";
 
 import config from "./config/index";
 import { typeDefs, resolvers } from "./schemas/index";
 
+// Create server express
 const app = express();
 
+// Constructor: schema definition and resolvers
 const server = new ApolloServer({
   typeDefs,
   resolvers
 });
 
+// Connection to MongoDB
+const mongoServer = new MongoMemoryServer();
+
+mongoose.Promise = Promise;
+mongoServer.getConnectionString().then(mongoUri => {
+  const mongooseOpts = {
+    autoReconnect: true,
+    reconnectTries: Number.MAX_VALUE,
+    reconnectInterval: 1000,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  };
+
+  mongoose.connect(mongoUri, mongooseOpts);
+
+  mongoose.connection.on("error", e => {
+    if (e.message.code === "ETIMEDOUT") {
+      console.log(e);
+      mongoose.connect(mongoUri, mongooseOpts);
+    }
+    console.log(e);
+  });
+
+  mongoose.connection.once("open", () => {
+    console.log(`🚀 Database running on ${mongoUri}`);
+  });
+});
+
+// Applying Apollo middleware
 server.applyMiddleware({ app });
 
 app.listen({ port: config.port }, () =>
@@ -18,3 +51,5 @@ app.listen({ port: config.port }, () =>
     `🚀 Server ready at http://localhost:${config.port}${server.graphqlPath}`
   )
 );
+
+export default app;
